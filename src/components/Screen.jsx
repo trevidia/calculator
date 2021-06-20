e9e0ace8ed37a7a5577a5ddaf6dcd81062552359
gemzy <trevidia@yahoo.com>
2021-06-20 19:53:30 +0100

    first deployment

---
+++ src/components/Screen.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import "./componentsStyles.css";
+
+class Screen extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div id="screen">
+                <div id="value1">
+
+                </div>
+                <div id="function">
+
+                </div>
+                <div id="value2">
+
+                </div>
+                <div id = "result">
+
+                </div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default Screen;
